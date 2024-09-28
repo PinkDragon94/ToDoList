3c49c3b3001b0aeaf85e5de353ed472506935a4e
Pinkdragon <cherandwhite@gmail.com>
2024-09-28 16:31:58 -0400

    update style

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
     return (
         <header>
-            <h1>To Do List</h1>
+            <h1>😍💞💘 𝓟𝓲𝓷𝓴𝓓𝓻𝓪𝓰𝓸𝓷𝓼 𝓣𝓸 𝓓𝓸 𝓛𝓲𝓼𝓽 💔💏💖</h1>
         </header>
     );
 }
