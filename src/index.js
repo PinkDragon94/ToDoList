78108abcc49e125908a8a4806db8741b5fbe13ba
Pinkdragon <cherandwhite@gmail.com>
2024-09-30 19:53:50 -0400

    Update

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// Import the service worker registration
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,5 +14,5 @@ root.render(
 
 reportWebVitals();
 
-serviceWorkerRegistration.register();
+
 
